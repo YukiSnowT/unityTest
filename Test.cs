a404c3c868ed82fa4c2978240ecfb5b7b9741d3a
kasukajima <zetta_minamimoto@softbank.ne.jp>
2021-11-22 13:28:41 +0900

    test

--- Test.cs
+++ Test.cs
@@ -2,16 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+    public class Player{
+        private int hp = 100;
+        private int power = 50;
+
+        public void Attack(){
+            Debug.Log(this.power + "のダメージを与えた");
+        }
+        public void Damage(int damage){
+            this.hp -= damage;
+            Debug.Log(damage + "のダメージを受けた");
+        }
+    }
+
+
 public class Test : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void sayHello(){
-        Debug.Log("Hello,World");
-    }
 
     void Start()
     {
-        sayHello();
+        Player myPlayer = new Player();
+        myPlayer.Attack();
+        myPlayer.Damage(30);
     }
 
     // Update is called once per frame
