0142f1088ce76e4272544687ad36153d2bd34f10
kasukajima <zetta_minamimoto@softbank.ne.jp>
2021-11-22 14:02:24 +0900

    test

--- Test.cs
+++ Test.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-    public class Player{
-        private int hp = 100;
-        private int power = 50;
+    // public class Player{
+    //     private int hp = 100;
+    //     private int power = 50;
 
-        public void Attack(){
-            Debug.Log(this.power + "のダメージを与えた");
-        }
-        public void Damage(int damage){
-            this.hp -= damage;
-            Debug.Log(damage + "のダメージを受けた");
-        }
-    }
+    //     public void Attack(){
+    //         Debug.Log(this.power + "のダメージを与えた");
+    //     }
+    //     public void Damage(int damage){
+    //         this.hp -= damage;
+    //         Debug.Log(damage + "のダメージを受けた");
+    //     }
+    // }
 
 
 public class Test : MonoBehaviour
@@ -21,9 +21,17 @@ public class Test : MonoBehaviour
 
     void Start()
     {
-        Player myPlayer = new Player();
-        myPlayer.Attack();
-        myPlayer.Damage(30);
+        Vector2 startPos = new Vector2(2.0f, 1.0f);
+        Vector2 endPos = new Vector2(8.0f, 5.0f);
+        Vector2 dir = endPos -startPos;
+        Debug.Log(dir);
+
+        float len = dir.magnitude;
+        Debug.Log(len);
+
+        // Player myPlayer = new Player();
+        // myPlayer.Attack();
+        // myPlayer.Damage(30);
     }
 
     // Update is called once per frame
